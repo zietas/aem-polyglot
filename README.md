4efd0680bf8f6328b62330af0468015bd1e19f14
Michał Ziętek <michal.zietek@contractors.roche.com>
2019-01-09 12:46:23 +0100

    fix for xml output formatting

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 [![Build Status](https://travis-ci.org/zietas/aem-polyglot.svg?branch=master)](https://travis-ci.org/zietas/aem-polyglot) 
 [![Coverage Status](https://coveralls.io/repos/github/zietas/aem-polyglot/badge.svg?branch=master)](https://coveralls.io/github/zietas/aem-polyglot?branch=master)
+[![Node version](https://img.shields.io/node/v/aem-polyglot.svg?style=flat)](http://nodejs.org/download/)
+[![dependencies Status](https://david-dm.org/zietas/aem-polyglot/status.svg)](https://david-dm.org/zietas/aem-polyglot)
+[![devDependencies Status](https://david-dm.org/zietas/aem-polyglot/dev-status.svg)](https://david-dm.org/zietas/aem-polyglot?type=dev)
+
 # AEM Polyglot
 
 This tool is designed mainly for developers who are tasked to translate automatically AEM dictionaries into other languages. 
@@ -14,10 +18,9 @@ In order to translate entries we are using Yandex Translation API. It's free to 
 
 On the other hand Google Translate API enforces user to provide credit card data on the spot in orde to get the key.   
 
-## Installation
+## Usage
 
-1. `npm install -g aem-polyglot`
-
+1. `npm install -g aem-polyglot` - install the package
 
 
 ## Usage
--- src/DictionaryHandler.js
+++ src/DictionaryHandler.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const converter = require('xml-js');
 const _ = require('lodash');
 
+const XML_DECLARATION = '<?xml version="1.0" encoding="UTF-8"?>';
+
 const XML_TO_JSON_OPTIONS = {
   compact: true
 };
@@ -9,15 +11,16 @@ const XML_TO_JSON_OPTIONS = {
 const JSON_TO_XML_OPTIONS = {
   compact: true,
   spaces: 4,
-  indentAttributes: true
+  indentAttributes: true,
+  ignoreDeclaration: true
 };
 
 class DictionaryHandler {
-  static exists (path) {
+  static exists(path) {
     return fs.existsSync(path);
   }
 
-  static async readDict (source) {
+  static async readDict(source) {
     return new Promise((resolve, reject) => {
       fs.readFile(source, 'utf-8', (err, data) => {
         if (err) {
@@ -34,8 +37,8 @@ class DictionaryHandler {
     });
   }
 
-  static async saveDict (jsonDict, target) {
-    function isValidDictionary (dict) {
+  static async saveDict(jsonDict, target) {
+    function isValidDictionary(dict) {
       return _.isObject(dict) && dict['_declaration'] && dict['jcr:root'];
     }
 
@@ -45,7 +48,10 @@ class DictionaryHandler {
           reject('Given dictionary is not an object');
           return;
         }
-        const xml = converter.js2xml(jsonDict, JSON_TO_XML_OPTIONS);
+        let xml = converter.js2xml(jsonDict, JSON_TO_XML_OPTIONS);
+        xml = XML_DECLARATION + xml;
+        xml = _.replace(xml, /\n[\ ]*\/\>/mg, '/>');
+        xml = _.replace(xml, /\n[\ ]*\>/mg, '>');
         fs.writeFile(target, xml, 'utf-8', (err) => {
           if (err) {
             reject(reject);
